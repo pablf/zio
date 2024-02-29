aac32de0f893b59558ac2114a5bca60d80e5bb08
pfeme <pablofeme@gmail.com>
2024-02-29 22:18:59 +0100

    add test

--- test-tests/shared/src/test/scala/zio/test/SmartAssertionSpec.scala
+++ test-tests/shared/src/test/scala/zio/test/SmartAssertionSpec.scala
@@ -493,6 +493,7 @@ object SmartAssertionSpec extends ZIOBaseSpec {
     ),
     suite("subtype seq")(
       test("compiles contains") {
+        import zio.test.SmartAssertions._
         val result = typeCheck {
         """
             val numbers = List(1,2)
@@ -500,9 +501,10 @@ object SmartAssertionSpec extends ZIOBaseSpec {
             assertTrue(numbers.forall(allNumbers.contains))
             """
         }
-        assertZIO(result)(isRight(test.Assertions.anything))
+        assertZIO(result)(isRight(anything))
       } @@ TestAspect.exceptScala212,
       test("compiles contains with Seq") {
+        import zio.test.SmartAssertions._
         val result = typeCheck {
         """
             val numbers = Seq(Seq[Any](1))
