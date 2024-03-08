4b3253751ed6dff1de91b7a81624c24266c5aaa3
pfeme <pablofeme@gmail.com>
2024-03-08 16:02:51 +0100

    exception

--- test/shared/src/main/scala-2/zio/test/SmartAssertMacros.scala
+++ test/shared/src/main/scala-2/zio/test/SmartAssertMacros.scala
@@ -327,9 +327,9 @@ $TestResult($ast.withCode($codeString).meta(location = $location))
     // `true` for conversion from `lhs` to `rhs`.
     def implicitConversionDirection(lhs: Type, rhs: Type): Option[Boolean] =
       if (tpesPriority(lhs) == -1 || tpesPriority(rhs) == -1) {
-        Try(c.inferImplicitValue((tq"$lhs => $rhs").tpe)).getOrElse(EmptyTree) match {
+        scala.util.Try(c.inferImplicitValue((tq"$lhs => $rhs").tpe)).getOrElse(EmptyTree) match {
           case EmptyTree => {
-            Try(c.inferImplicitValue((tq"$rhs => $lhs").tpe)).getOrElse(EmptyTree) match {
+            scala.util.Try(c.inferImplicitValue((tq"$rhs => $lhs").tpe)).getOrElse(EmptyTree) match {
               case EmptyTree => None
               case _         => Some(false)
             }
