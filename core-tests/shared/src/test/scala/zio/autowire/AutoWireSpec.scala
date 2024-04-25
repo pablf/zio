78d2ced17c0627451193a5cca1d916a74db00036
pfeme <pablofeme@gmail.com>
2024-04-25 19:47:17 +0200

    lllk

--- core-tests/shared/src/test/scala/zio/autowire/AutoWireSpec.scala
+++ core-tests/shared/src/test/scala/zio/autowire/AutoWireSpec.scala
@@ -259,7 +259,7 @@ object AutoWireSpec extends ZIOBaseSpec {
           },
           test("graph"){
             import zio.internal.macros._
-            def mkGraph(layers: Node[String, String], in: List[String], out: List[String]): String = {
+            def mkGraph(layers: List[Node[String, String]], in: List[String], out: List[String]): String = {
               val eq: (String, String) => Boolean = {_ == _}
               val env: String => Node[String, String] = {k => Node(List(k), List(k), s"env[$k]")}
               val deps = in.map(env)
