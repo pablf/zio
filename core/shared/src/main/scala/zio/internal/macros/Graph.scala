a3fb5cf833675944de4f652a4a0ec6e3ceb5a9f8
pfeme <pablofeme@gmail.com>
2024-05-05 18:11:11 +0200

    af

--- core/shared/src/main/scala/zio/internal/macros/Graph.scala
+++ core/shared/src/main/scala/zio/internal/macros/Graph.scala
@@ -17,13 +17,14 @@ final case class Graph[Key, A](
   private var envDependencies: List[Key] = Nil
 
   def buildNodes(outputs: List[Key], sideEffectNodes: List[Node[Key, A]]): Either[::[GraphError[Key, A]], LayerTree[A]] = for {
-    _ <- Right(println(s"should be true: ${envKeys.headOption.map(isEnv(_))}"))
+    //_ <- Right(println(s"should be true: ${envKeys.headOption.map(isEnv(_))}"))
     //_ <- Right(println(s"called with ${outputs.toString} and ${nodes.toString} and ${sideEffectNodes.toString} "))
     _           <- neededKeys(distinctKeys(outputs ++ sideEffectNodes.flatMap(_.inputs)))
-    _ <- Right(println(neededKeys.toString))
+    //_ <- Right(println(neededKeys.toString))
     sideEffects <- forEach(sideEffectNodes)(buildNode).map(_.combineHorizontally)
     rightTree   <- build(outputs)
     leftTree    <- buildComplete(constructDeps())
+    _ <- Right(println(s"with output $outputs, $sideEffects, $rightTree, $leftTree"))
   } yield leftTree >>> (rightTree ++ sideEffects)
 
 
