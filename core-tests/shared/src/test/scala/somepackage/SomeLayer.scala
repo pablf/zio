1756b50177b1517b45e335605816b007c343b537
pfeme <pablofeme@gmail.com>
2024-05-18 12:21:28 +0200

    DDD

--- core-tests/shared/src/test/scala/somepackage/SomeLayer.scala
+++ core-tests/shared/src/test/scala/somepackage/SomeLayer.scala
@@ -1,11 +1,11 @@
 package somepackage
 
-import zio.ZLayer
+import zio._
 
 object SomeLayer {
 
   case class A()
 
-  val live: ZLayer[otherpackage.B, Nothing, A] = ???
+  val live: ZLayer[otherpackage.B, Nothing, A] = ZLayer { ZIO.service[otherpackage.B].map(_ => A()) }
 
 }
