4cf2f7f5b0f12ea16125832409f51bae5ca4831f
pfeme <pablofeme@gmail.com>
2024-05-21 00:02:41 +0200

    lkjlkj

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
@@ -3813,7 +3813,7 @@ object ZStreamSpec extends ZIOBaseSpec {
 
             val effectAll = (stream1 merge stream2).runDrain
             for
-              _ <- zio.test.Live.live(effectAll)/*
+              _ <- zio.test.Live.live(effectAll.catchAllDefect(_ => ZIO.die(new Throwable("in test"))))/*
                 .catchAllDefect(ZIO.fail(_))
                 .tapErrorCause(ZIO.logErrorCause("mmm sweet", _))*/
               _ <- TestClock.adjust(1.second)
