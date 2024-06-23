b32e4abe26d92a48c85893750829451abecfc693
pfeme <pablofeme@gmail.com>
2024-06-23 22:19:55 +0200

    hh

--- test/shared/src/main/scala-2/zio/test/SmartAssertMacros.scala
+++ test/shared/src/main/scala-2/zio/test/SmartAssertMacros.scala
@@ -53,7 +53,7 @@ class SmartAssertMacros(val c: blackbox.Context) {
     args: List[c.Tree] = List.empty,
     implicits: Boolean = false
   ) {
-    private[AssertAST] def this(name: String, tpes: List[Type], args: List[c.Tree]): AssertAST =
+    private[AssertAST] def this(name: String, tpes: List[Type], args: List[c.Tree]) =
       this(name, tpes, args, false)
     private[AssertAST] def copy(name: String = name, tpes: List[Type] = tpes, args: List[c.Tree] = args): AssertAST =
       AssertAST(name, tpes, args, implicits)
